c485a1c094e00668776e573ffe1ae7fa5daf194a
s3432733 <s3432733@student.rmit.edu.au>
2019-03-14 21:49:53 +1100

    first commit

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\jon_s\docume~1\rmit\bp094b~1\cosc24~4\stopover\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
