948e2fd0db665c629c09dd99691ca3f755b07546
NotKhoa <khoa.nicky@gmail.com>
2019-03-15 18:50:45 +1100

    linux

--- env/bin/django-admin.py
+++ env/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/Users/Jtan/Study/RMIT/2019Sem1/Capstone/StopOver/env/bin/python3.7
+#!/home/khoa/Desktop/2019_SEM1/Programming Project 1/StopOver/env/bin/python3
 from django.core import management
 
 if __name__ == "__main__":
