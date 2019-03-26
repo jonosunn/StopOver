ed5255b09a3902bd5d0f0923a4af9167878f9b8a
Envying <jon_sun20@hotmail.com>
2019-03-26 16:05:17 +1100

    created a get_mylocation function

--- map/views.py
+++ map/views.py
@@ -3,3 +3,10 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'map/homepage.html')
+
+def get_mylocation(request):
+    if request.GET.get('find-me'):
+        longitude = request.COOKIES.get('longitude', '')
+        print(longitude)
+        print("test")
+    return longitude
