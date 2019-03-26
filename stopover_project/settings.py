d49a17c2e5d3935bfbaaa75fc54996f320c616f9
TyuioPi <j17tan@gmail.com>
2019-03-26 17:55:20 +1100

    Updated django db

--- stopover_project/settings.py
+++ stopover_project/settings.py
@@ -127,4 +127,5 @@ django_heroku.settings(locals())
 
 # Parse values of DATABASE_URL and convert for django readability
 import dj_database_url
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['default'].update(db_from_env)
