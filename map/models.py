1e12afa72863595c179bca80aacec2a31b33db93
TyuioPi <j17tan@gmail.com>
2019-03-29 15:17:30 +1100

    Add car model to db

--- map/models.py
+++ map/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Car(models.Model):
+	brand = models.CharField(max_length=20)
+	transmission = models.CharField(max_length=20)
+	numberplate = models.CharField(max_length=7)
+	price = models.IntegerField()
+	longitude = models.FloatField()
+	latitude = models.FloatField()
+	availble = models.BooleanField(default=True)
+
+	def __init__
