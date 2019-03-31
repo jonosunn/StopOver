2b11c94336bef2fe3b81d518dbf1bf2c97de5b87
TyuioPi <j17tan@gmail.com>
2019-03-31 16:45:57 +1100

    Added commenting and displayed the car list to the view

--- map/views.py
+++ map/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from map.models import Car
 
 def home(request):
-    return render(request, 'map/homepage.html')
+	# Get all car objects from db
+	cars = Car.objects.all()
+
+	# Argument to contain list of our car model
+	args = {'cars': cars}
+	return render(request, 'map/homepage.html', args)
