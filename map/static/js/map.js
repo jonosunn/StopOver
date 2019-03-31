f97fcf8681ccdbb9549d3e72d278cf338e0b168e
s3432733 <s3432733@student.rmit.edu.au>
2019-03-31 22:24:52 +1100

    changed central location to user

--- map/static/js/map.js
+++ map/static/js/map.js
@@ -3,24 +3,6 @@ function geoFindMe() {
 
   // document.querySelector('#find-me').addEventListener('click', geoFindMe);
 
-  // Mapbox script to display map, also uses to edit the center and zoom etc
-  mapboxgl.accessToken = 'pk.eyJ1Ijoic3RvcG92ZXJhZG1pbiIsImEiOiJjanRtcHY3YXozeW10NGJvM3c2dWlxZ2xvIn0.1WlrGXizCLdOrV5TXPTc0A';
-  //set bounds of melbourne
-  //format: [south, west], [north, east]
-  //follow mapbox's coordinates system
-  var bounds = [
-   	 [144.70345,-37.91802],
-   	 [145.22873,-37.64668]
-  ];
-    var map = new mapboxgl.Map({
-      container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: [144.97, -37.81],
-      zoom: 12,
-      bearing: 0,
-      maxBounds: bounds
-  });
-
   const status = document.querySelector('#status');
   const mapLink = document.querySelector('#map-link');
 
@@ -37,6 +19,25 @@ function geoFindMe() {
   function success(position) {
     const latitude  = position.coords.latitude;
     const longitude = position.coords.longitude;
+
+
+      // Mapbox script to display map, also uses to edit the center and zoom etc
+      mapboxgl.accessToken = 'pk.eyJ1Ijoic3RvcG92ZXJhZG1pbiIsImEiOiJjanRtcHY3YXozeW10NGJvM3c2dWlxZ2xvIn0.1WlrGXizCLdOrV5TXPTc0A';
+      //set bounds of melbourne
+      //format: [south, west], [north, east]
+      //follow mapbox's coordinates system
+      var bounds = [
+       	 [144.70345,-37.91802],
+       	 [145.22873,-37.64668]
+      ];
+        var map = new mapboxgl.Map({
+          container: 'map',
+          style: 'mapbox://styles/mapbox/streets-v11',
+          center: [longitude, latitude],
+          zoom: 12,
+          bearing: 0,
+          maxBounds: bounds
+      });
 
     status.textContent = '';
     mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
@@ -72,39 +73,7 @@ function geoFindMe() {
         .addTo(map);
     });
 
-
-    // add car markers to map
-    geojson_carlist.features.forEach(function(marker) {
-      // create a HTML element for each feature
-      var el = document.createElement('div');
-      el.className = 'marker';
-      // make a marker for each feature and add to the map
-      new mapboxgl.Marker(el)
-        .setLngLat(marker.geometry.coordinates)
-        .addTo(map);
-    });
-
-
-    // loading marker for the car Location
-        var geojson_carlist = {
-          type: 'FeatureCollection',
-          features: [
-            (% for car in cars %)
-            {
-            type: 'Feature',
-            geometry: {
-              type: 'Point',
-              coordinates: [{{ car.longitude }}, {{ car.latitude }}]
-            },
-            properties: {
-              title: 'Mapbox',
-              description: 'Car Locations'
-            }
-          },
-        {% endfor %}
-        ]
-        };
-  }
+}
 
   function error() {
     status.textContent = 'Unable to retrieve your location';
