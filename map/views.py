1db35eac2ea4e939daae7067a74520bab0d37d29
NotKhoa <khoa.nicky@gmail.com>
2019-04-03 17:58:55 +1100

    add book function

--- map/views.py
+++ map/views.py
@@ -6,8 +6,13 @@ def home(request):
 	# Get all car objects from db
 	cars = Car.objects.filter(available=True)
 
+	if request.method == "POST":
+		car_number_plate = request.POST.get('number_plate_id', None) # Set number_plate value
+		set_car = Car.objects.get(number_plate=car_number_plate) # Set Car
+		set_car.available = False # Change available to False
+		set_car.save()
 	# Argument to contain list of our car model
-	args = {'cars': cars}
+	args = {'cars': cars }
 	return render(request, 'map/homepage.html', args)
 
 def get_mylocation(request):
