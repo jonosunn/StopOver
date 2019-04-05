c87dea267541ec70dfd8137765033d35fa422a74
s3432733 <s3432733@student.rmit.edu.au>
2019-04-05 15:25:16 +1100

    added testSample cases

--- map/tests.py
+++ map/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.http import HttpRequest
+from django.test import SimpleTestCase
+from django.urls import reverse
+
+from . import views
+
+class HomePageTest(SimpleTestCase):
+
+    def test_home_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEquals(response.status.code, 200)
 
 # Create your tests here.
