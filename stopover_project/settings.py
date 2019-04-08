0ed0970b0443c926ddb2e4e06b8ff58f53cd654e
TyuioPi <j17tan@gmail.com>
2019-04-08 21:03:29 +1000

    Default database to sqlite3 when testing

--- stopover_project/settings.py
+++ stopover_project/settings.py
@@ -62,12 +62,12 @@ WSGI_APPLICATION = 'stopover_project.wsgi.application'
 
 # Database
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 DATABASES = {
     'default': {
@@ -94,7 +94,7 @@ DATABASES = {
 #     }
 # }
 
-TEST_RUNNER = 'map.test_suite_runner.HerokuDiscoverRunner'
+# TEST_RUNNER = 'map.test_suite_runner.HerokuDiscoverRunner'
 
 # Password validation
 
@@ -142,5 +142,10 @@ django_heroku.settings(locals())
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
 DATABASES['default'].update(db_from_env)
-DATABASES['default']['TEST'].update(db_from_env)
+# DATABASES['default']['TEST'].update(db_from_env)
+
+# Use sqlite3 database when performing unit tests
+import sys
+if 'test' in sys.argv or 'test_coverage' in sys.argv:
+    DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
 
