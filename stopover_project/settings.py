14ce88b966ec0cc9b7e0967116d3333ba9372024
TyuioPi <j17tan@gmail.com>
2019-04-10 08:58:16 +1000

    Remove settings for sqlite3 db

--- stopover_project/settings.py
+++ stopover_project/settings.py
@@ -62,12 +62,12 @@ WSGI_APPLICATION = 'stopover_project.wsgi.application'
 
 # Database
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 DATABASES = {
     'default': {
