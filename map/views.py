529dfbc0b71cb4428ba4ffc016e1d6b2366eb927
TyuioPi <j17tan@gmail.com>
2019-04-09 18:19:49 +1000

    Added db testing

--- map/views.py
+++ map/views.py
@@ -5,12 +5,12 @@ from django.views.generic import TemplateView, View
 from django.views.generic.list import ListView
 
 class HomePageView(TemplateView):
-    template_name = 'map/homepage.html'
+	template_name = 'map/homepage.html'
 
-    def get_context_data(self, *args, **kwargs):
-        context = super(HomePageView, self).get_context_data(*args, **kwargs)
-        context['cars'] = Car.objects.filter(available=True)
-        return context
+	def get_context_data(self, *args, **kwargs):
+		context = super(HomePageView, self).get_context_data(*args, **kwargs)
+		context['cars'] = Car.objects.filter(available=True)
+		return context
 
 # class HomePageView(ListView):
 #     template_name = 'map/homepage.html'
