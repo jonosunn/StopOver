81597aefc9bdd0f49160c4c670ee1cffcfb1a0c9
s3432733 <s3432733@student.rmit.edu.au>
2019-04-09 13:22:40 +1000

    fixed templateView

--- map/views.py
+++ map/views.py
@@ -4,13 +4,13 @@ from map.models import Car
 from django.views.generic import TemplateView, View
 from django.views.generic.list import ListView
 
-# class HomePageView(TemplateView):
-#     template_name = 'map/homepage.html'
-#
-#     def get_content_data(self, *args, **kwargs):
-#         context = super(HomePageView, self).get_content_data(*args, **kwargs)
-#         context['cars'] = Car.objects.filter(available=True)
-#         return context
+class HomePageView(TemplateView):
+    template_name = 'map/homepage.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(HomePageView, self).get_context_data(*args, **kwargs)
+        context['cars'] = Car.objects.filter(available=True)
+        return context
 
 # class HomePageView(ListView):
 #     template_name = 'map/homepage.html'
@@ -20,16 +20,16 @@ from django.views.generic.list import ListView
 #     def get_queryset(self):
 #         return Car.objects.filter(available=True)
 
-class HomePageView(View):
-    template_name = 'map/homepage.html'
-
-    def get(self, request, *args, **kwargs):
-        # Get all car objects from db
-        cars = Car.objects.filter(available=True)
-
-        # Argument to contain list of our car model
-        context = {'cars': cars}
-        return render(request, self.template_name, context=context)
+# class HomePageView(View):
+#     template_name = 'map/homepage.html'
+#
+#     def get(self, request, *args, **kwargs):
+#         # Get all car objects from db
+#         cars = Car.objects.filter(available=True)
+#
+#         # Argument to contain list of our car model
+#         context = {'cars': cars}
+#         return render(request, self.template_name, context=context)
 
 # def get_mylocation(request):
 #     if request.GET.get('find-me'):
