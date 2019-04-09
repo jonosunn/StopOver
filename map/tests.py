3fc54f63084297a3ae36cd1ea2c6c7374f2ebce5
s3432733 <s3432733@student.rmit.edu.au>
2019-04-09 13:55:18 +1000

    added more test

--- map/tests.py
+++ map/tests.py
@@ -10,6 +10,15 @@ class HomePageTest(TestCase):
         response = self.client.get('')
         self.assertEquals(response.status_code, 200)
 
+    def test_view_url_by_name(self):
+        response = self.client.get(reverse('home'))
+        self.assertEquals(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('home'))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'map/homepage.html')
+
     def test_home_page_contains_correct_html(self):
         response = self.client.get('')
         self.assertContains(response, '<title>Homepage</title>')
