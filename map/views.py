ef06a603c0baac7a0211c996d68c4f459b6e7cb9
NotKhoa <khoa.nicky@gmail.com>
2019-04-10 20:11:52 +1000

    clean up

--- map/views.py
+++ map/views.py
@@ -6,15 +6,9 @@ from map.forms import CarForm
 def home(request):
 	# Get all car objects from db
 	cars = Car.objects.all().filter(available=True)
+	# Argument to contain list of our car model
 	args = {'cars': cars }
-	# if request.method == "POST":
-	# 	car_number_plate = request.POST.get('number_plate_id', None) # Set number_plate value
-	# 	set_car = Car.objects.get(number_plate=car_number_plate) # Set Car
-	# 	# set_car.available = False # Change available to False
-	# 	set_car.save()
-	# 	redirect('map/confirmation.html')
-	# else:
-		# Argument to contain list of our car model
+
 
 	return render(request, 'map/homepage.html', args)
 
@@ -40,11 +34,8 @@ def car_detail_view(request, id):
 			print(form.errors)
 	else:
 		car = Car.objects.get(id=id)
-		form = CarForm()
-		form.fields['brand'].initial = car.brand
-		form.fields['number_plate'].initial = car.number_plate
-		form.fields['price'].initial = car.price
-		form.fields['available'].initial = car.available
+		form = CarForm(initial={'brand':car.brand, 'number_plate':car.number_plate, 'price':car.price,
+							'available':car.available})
 		args = {
 			'car':car,
 			'form':form
