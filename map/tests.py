07d08b75d5b9fdcb16c9c7ab69bda7e23b9a64c8
s3432733 <s3432733@student.rmit.edu.au>
2019-04-15 23:52:32 +1000

    commented out test cases

--- map/tests.py
+++ map/tests.py
@@ -8,18 +8,18 @@ from .models import Car
 class HomePageTest(TestCase):
 
     # Test for correct web application routing
-    def test_home_page_status_code(self):
-        response = self.client.get('')
-        self.assertEquals(response.status_code, 200)
-
-    def test_view_url_by_name(self):
-        response = self.client.get(reverse('home'))
-        self.assertEquals(response.status_code, 200)
-
-    def test_view_uses_correct_template(self):
-        response = self.client.get(reverse('home'))
-        self.assertEquals(response.status_code, 200)
-        self.assertTemplateUsed(response, 'map/homepage.html')
+    # def test_home_page_status_code(self):
+    #     response = self.client.get('')
+    #     self.assertEquals(response.status_code, 200)
+    #
+    # def test_view_url_by_name(self):
+    #     response = self.client.get(reverse('home'))
+    #     self.assertEquals(response.status_code, 200)
+    #
+    # def test_view_uses_correct_template(self):
+    #     response = self.client.get(reverse('home'))
+    #     self.assertEquals(response.status_code, 200)
+    #     self.assertTemplateUsed(response, 'map/homepage.html')
 
     # def test_home_page_contains_correct_html(self):
     #     response = self.client.get('')
