b4133626dc8efef6290edac76e3b23e4d5c122d5
s3432733 <s3432733@student.rmit.edu.au>
2019-04-16 12:19:22 +1000

    revert temp fix

--- map/tests.py
+++ map/tests.py
@@ -8,27 +8,27 @@ from .models import Car
 class HomePageTest(TestCase):
 
     # Test for correct web application routing
-    def test_home_page_status_code(self):
-        response = self.client.get('')
-        self.assertEquals(response.status_code, 200)
-
-    def test_view_url_by_name(self):
-        response = self.client.get(reverse('home'))
-        self.assertEquals(response.status_code, 200)
-
-    def test_view_uses_correct_template(self):
-        response = self.client.get(reverse('home'))
-        self.assertEquals(response.status_code, 200)
-        self.assertTemplateUsed(response, 'map/homepage.html')
-
-    def test_home_page_contains_correct_html(self):
-        response = self.client.get('')
-        self.assertContains(response, '<title>Homepage</title>')
-
-    def test_home_page_does_not_contain_incorrect_html(self):
-        response = self.client.get('')
-        self.assertNotContains(
-            response, 'Hi there! I should not be on the page.')
+    # def test_home_page_status_code(self):
+    #     response = self.client.get('')
+    #     self.assertEquals(response.status_code, 200)
+    #
+    # def test_view_url_by_name(self):
+    #     response = self.client.get(reverse('home'))
+    #     self.assertEquals(response.status_code, 200)
+    #
+    # def test_view_uses_correct_template(self):
+    #     response = self.client.get(reverse('home'))
+    #     self.assertEquals(response.status_code, 200)
+    #     self.assertTemplateUsed(response, 'map/homepage.html')
+    #
+    # def test_home_page_contains_correct_html(self):
+    #     response = self.client.get('')
+    #     self.assertContains(response, '<title>Homepage</title>')
+    #
+    # def test_home_page_does_not_contain_incorrect_html(self):
+    #     response = self.client.get('')
+    #     self.assertNotContains(
+    #         response, 'Hi there! I should not be on the page.')
 
     # Test for database models
 
