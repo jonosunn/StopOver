ea32fe336072bea26f7197e9998adc61ac917567
apcteo <ateo@builtbit.com>
2019-04-22 22:07:33 +1000

    Add new views and new settings in views.py/urls/py for Paypal payment

--- map/urls.py
+++ map/urls.py
@@ -6,4 +6,6 @@ from django.contrib.auth.decorators import login_required
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('confirmation/<slug:number_plate>', login_required(ConfirmationPage.as_view()), name='confirmation'),
+    path('confirmation/paysubmit', ConfirmationPage.as_view(), name='process_payment'),
+    path('confirmation/paysuccess', ConfirmationPage.as_view(), name='payment_done'),
 ]
--- map/views.py
+++ map/views.py
@@ -14,7 +14,7 @@ from map.forms import CarForm
 class HomePageView(TemplateView):
 	template_name = 'map/homepage.html'
 
-	def get_context_data(self, request):
+	def get_context_data(self, *args, **kwargs):
 		print("get_context_data")
 		numberplate = self.request.GET.get("number_plate")
 		if numberplate != None:
@@ -22,55 +22,6 @@ class HomePageView(TemplateView):
 		context = super(HomePageView, self).get_context_data(*args, **kwargs)
 		context['cars'] = Car.objects.filter(available=True)
 		return context
-	
-	#TODO:
-	def checkout(request):
-		if request.method == 'POST':
-			form = CheckoutForm(request.POST)
-			if form.is_valid():
-				cleaned_data = form.cleaned_data
-	       #...
-	       #...
-		        cart.clear(request)
-		        request.session['order_id'] = o.id
-		        return redirect('process_payment')
-		else:
-			form = CheckoutForm()
-	    	return (render(request, '', locals()))
-        
-	#TODO: SET CORRECT URLS
-	def process_payment(self):
-		numberplate = self.request.GET.get("number_plate")
-		if numberplate != None:
-			set_car = Car.objects.get(number_plate=numberplate) 
-			
-		paypal_dict = {
-	        'business': settings.PAYPAL_RECEIVER_EMAIL,
-	        'amount': '%.2f' % set_car.price().quantize(
-	            Decimal('.01')),
-	        'item_name': 'Order {}'.format(numberplate),
-	        'invoice': str(numberplate),
-	        'currency_code': 'AUD',
-	        'notify_url': 'http://{}{}'.format(host,
-	                                           reverse('paypal-ipn')),
-	        'return_url': 'http://{}{}'.format(host,
-	                                           reverse('payment_done')),
-	        'cancel_return': 'http://{}{}'.format(host,
-	                                              reverse('payment_cancelled')),
-	    }
-	 	form = PayPalPaymentsForm(initial=paypal_dict)
-	 	return render(request, 'map/process_payment.html', {'order': car, 'form': form})
-
-	#TODO: SET CORRECT URLS
-	@csrf_exempt
-	def payment_done(request):
-	    return render(request, '')
-	 
-	#TODO: SET CORRECT URLS
-	@csrf_exempt
-	def payment_canceled(request):
-	    return render(request, '')
-	    return render(request, '')
 
 class ConfirmationPage(TemplateView):
 	template_name = 'confirmation/confirmation.html'
@@ -82,3 +33,32 @@ class ConfirmationPage(TemplateView):
         	"car": set_car
     	}
 		return render(request, self.template_name, args)
+	
+	#TODO: SET CORRECT URLS
+	def process_payment(self):
+		numberplate = self.request.GET.get("number_plate")
+		if numberplate != None:
+			set_car = Car.objects.get(number_plate=numberplate) 
+			
+		host = request.get_host();
+		
+		paypal_dict = {
+	        'business': settings.PAYPAL_RECEIVER_EMAIL,
+	        'amount': '%.2f' % set_car.price().quantize(
+	            Decimal('.01')),
+	        'item_name': 'Order {}'.format(set_car.id),
+	        'invoice': str(numberplate),
+	        'currency_code': 'AUD',
+	        'return_url': 'http://{}{}'.format(host,
+	                                           reverse('payment_done')),
+	        'cancel_return': 'http://{}{}'.format(host,
+	                                              reverse('payment_cancelled')),
+	    }
+		
+		form = PayPalPaymentsForm(initial=paypal_dict)
+		return render(request, 'confirmation/paysuccess.html', {'order': set_car, 'form': form})
+	#TODO: SET CORRECT URLS
+	@csrf_exempt
+	def payment_done(request):
+	    return render(request, 'confirmation/paysuccess.html')
+	
