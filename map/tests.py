bafa1f53b1f3dc89fdc23c4e2403a5ef30252aaa
s3432733 <s3432733@student.rmit.edu.au>
2019-04-30 12:19:50 +1000

    temp commented out routing

--- map/tests.py
+++ map/tests.py
@@ -30,27 +30,27 @@ class MapAppTest(TestCase):
         self.assertNotContains(
             response, 'Hi there! I should not be on the page.')
 
-    def test_booking_page_status_code(self):
-        response = self.client.get(reverse('confirmation'))
-        self.assertEquals(response.status_code, 200)
-
-    def test_booking_url_by_name(self):
-        response = self.client.get(reverse('confirmation'))
-        self.assertEquals(response.status_code, 200)
-
-    def test_booking_uses_correct_template(self):
-        response = self.client.get(reverse('confirmation'))
-        self.assertEquals(response.status_code, 200)
-        self.assertTemplateUsed(response, 'confirmation/confirmation.html')
-
-    def test_booking_page_contains_correct_html(self):
-        response = self.client.get(reverse('confirmation'))
-        self.assertContains(response, '<title>Booking</title>')
-
-    def test_booking_page_does_not_contain_incorrect_html(self):
-        response = self.client.get(reverse('confirmation'))
-        self.assertNotContains(
-            response, 'Hi there! I should not be on the page.')
+    # def test_booking_page_status_code(self):
+    #     response = self.client.get(reverse('confirmation'))
+    #     self.assertEquals(response.status_code, 200)
+    #
+    # def test_booking_url_by_name(self):
+    #     response = self.client.get(reverse('confirmation'))
+    #     self.assertEquals(response.status_code, 200)
+    #
+    # def test_booking_uses_correct_template(self):
+    #     response = self.client.get(reverse('confirmation'))
+    #     self.assertEquals(response.status_code, 200)
+    #     self.assertTemplateUsed(response, 'confirmation/confirmation.html')
+    #
+    # def test_booking_page_contains_correct_html(self):
+    #     response = self.client.get(reverse('confirmation'))
+    #     self.assertContains(response, '<title>Booking</title>')
+    #
+    # def test_booking_page_does_not_contain_incorrect_html(self):
+    #     response = self.client.get(reverse('confirmation'))
+    #     self.assertNotContains(
+    #         response, 'Hi there! I should not be on the page.')
 
 
 
