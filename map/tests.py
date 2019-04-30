49fe180dc914a2cdbbc9e3b0e78c5ff5917f86e1
s3432733 <s3432733@student.rmit.edu.au>
2019-04-30 22:51:26 +1000

    added slug testing, missing PAYPAL email reciever for those routing

--- map/tests.py
+++ map/tests.py
@@ -7,72 +7,6 @@ from .models import Car
 
 class MapAppTest(TestCase):
 
-    # Test for correct web application routing
-    def test_home_page_status_code(self):
-        response = self.client.get('')
-        self.assertEquals(response.status_code, 200)
-
-    def test_home_url_by_name(self):
-        response = self.client.get(reverse('home'))
-        self.assertEquals(response.status_code, 200)
-
-    def test_home_uses_correct_template(self):
-        response = self.client.get(reverse('home'))
-        self.assertEquals(response.status_code, 200)
-        self.assertTemplateUsed(response, 'map/homepage.html')
-
-    def test_home_page_contains_correct_html(self):
-        response = self.client.get('')
-        self.assertContains(response, '<title>Homepage</title>')
-
-    def test_home_page_does_not_contain_incorrect_html(self):
-        response = self.client.get('')
-        self.assertNotContains(
-            response, 'Hi there! I should not be on the page.')
-
-    # def test_booking_page_status_code(self):
-    #     response = self.client.get(reverse('confirmation'))
-    #     self.assertEquals(response.status_code, 200)
-    #
-    # def test_booking_url_by_name(self):
-    #     response = self.client.get(reverse('confirmation'))
-    #     self.assertEquals(response.status_code, 200)
-    #
-    # def test_booking_uses_correct_template(self):
-    #     response = self.client.get(reverse('confirmation'))
-    #     self.assertEquals(response.status_code, 200)
-    #     self.assertTemplateUsed(response, 'confirmation/confirmation.html')
-    #
-    # def test_booking_page_contains_correct_html(self):
-    #     response = self.client.get(reverse('confirmation'))
-    #     self.assertContains(response, '<title>Booking</title>')
-    #
-    # def test_booking_page_does_not_contain_incorrect_html(self):
-    #     response = self.client.get(reverse('confirmation'))
-    #     self.assertNotContains(
-    #         response, 'Hi there! I should not be on the page.')
-
-    def test_confirmation_page_status_code(self):
-        response = self.client.get(reverse('payment_done'))
-        self.assertEquals(response.status_code, 200)
-
-    def test_confirmation_url_by_name(self):
-        response = self.client.get(reverse('payment_done'))
-        self.assertEquals(response.status_code, 200)
-
-    def test_confirmation_uses_correct_template(self):
-        response = self.client.get(reverse('payment_done'))
-        self.assertEquals(response.status_code, 200)
-        self.assertTemplateUsed(response, 'confirmation/paysuccess.html')
-
-    def test_confirmation_page_contains_correct_html(self):
-        response = self.client.get(reverse('payment_done'))
-        self.assertContains(response, '<title>Pay Success</title>')
-
-    def test_confirmation_page_does_not_contain_incorrect_html(self):
-        response = self.client.get(reverse('payment_done'))
-        self.assertNotContains(
-            response, 'Hi there! I should not be on the page.')
 
     # Test for database models
 
@@ -109,3 +43,76 @@ class MapAppTest(TestCase):
         self.assertIsInstance(car.longitude, float)
         self.assertIsInstance(car.latitude, float)
         self.assertIsInstance(car.available, bool)
+
+
+    # Test for correct web application routing
+    def test_home_page_status_code(self):
+        response = self.client.get('')
+        self.assertEquals(response.status_code, 200)
+
+    def test_home_url_by_name(self):
+        response = self.client.get(reverse('home'))
+        self.assertEquals(response.status_code, 200)
+
+    def test_home_uses_correct_template(self):
+        response = self.client.get(reverse('home'))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'map/homepage.html')
+
+    def test_home_page_contains_correct_html(self):
+        response = self.client.get('')
+        self.assertContains(response, '<title>Homepage</title>')
+
+    def test_home_page_does_not_contain_incorrect_html(self):
+        response = self.client.get('')
+        self.assertNotContains(
+            response, 'Hi there! I should not be on the page.')
+
+    # def test_booking_page_status_code(self):
+    #     car = Car.objects.get(id=1)
+    #     response = self.client.get(reverse('confirmation', args=(car.number_plate,)))
+    #     self.assertEquals(response.status_code, 200)
+    #
+    # def test_booking_url_by_name(self):
+    #     car = Car.objects.get(id=1)
+    #     response = self.client.get(reverse('confirmation', args=(car.number_plate,)))
+    #     self.assertEquals(response.status_code, 200)
+    #
+    # def test_booking_uses_correct_template(self):
+    #     car = Car.objects.get(id=1)
+    #     response = self.client.get(reverse('confirmation', args=(car.number_plate,)))
+    #     self.assertEquals(response.status_code, 200)
+    #     self.assertTemplateUsed(response, 'confirmation/confirmation.html')
+    #
+    # def test_booking_page_contains_correct_html(self):
+    #     car = Car.objects.get(id=1)
+    #     response = self.client.get(reverse('confirmation', args=(car.number_plate,)))
+    #     self.assertContains(response, '<title>Booking</title>')
+    #
+    # def test_booking_page_does_not_contain_incorrect_html(self):
+    #     car = Car.objects.get(id=1)
+    #     response = self.client.get(reverse('confirmation', args=(car.number_plate,)))
+    #     self.assertNotContains(
+    #         response, 'Hi there! I should not be on the page.')
+
+    def test_confirmation_page_status_code(self):
+        response = self.client.get(reverse('payment_done'))
+        self.assertEquals(response.status_code, 200)
+
+    def test_confirmation_url_by_name(self):
+        response = self.client.get(reverse('payment_done'))
+        self.assertEquals(response.status_code, 200)
+
+    def test_confirmation_uses_correct_template(self):
+        response = self.client.get(reverse('payment_done'))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'confirmation/paysuccess.html')
+
+    def test_confirmation_page_contains_correct_html(self):
+        response = self.client.get(reverse('payment_done'))
+        self.assertContains(response, '<title>Pay Success</title>')
+
+    def test_confirmation_page_does_not_contain_incorrect_html(self):
+        response = self.client.get(reverse('payment_done'))
+        self.assertNotContains(
+            response, 'Hi there! I should not be on the page.')
