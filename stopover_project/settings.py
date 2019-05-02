f3a7ea58a692a06fd72e167288f044bece05882b
s3432733 <s3432733@student.rmit.edu.au>
2019-05-02 22:17:15 +1000

    test

--- stopover_project/settings.py
+++ stopover_project/settings.py
@@ -40,7 +40,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'stopover_project.urls'
@@ -132,7 +131,7 @@ USE_TZ = True
 SESSION_EXPIRE_SECONDS = 10*60 # 10 minutes session timer
 
 # Static files (CSS, JavaScript, Images)
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
