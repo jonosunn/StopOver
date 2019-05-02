7495b45829febaea4d325ce73787f9dea455da64
s3432733 <s3432733@student.rmit.edu.au>
2019-05-02 23:54:52 +1000

    test

--- stopover_project/settings.py
+++ stopover_project/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'exhlfdat&vfum(-34*c2uroi(($ww(yo$9pv98=e6p^gl(-eoj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*'] # May need to be changed
 
