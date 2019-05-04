49f83ce547311406f7018600588cb54fc754df91
s3432733 <s3432733@student.rmit.edu.au>
2019-05-04 23:01:57 +1000

    code clean up

--- booking/views.py
+++ booking/views.py
@@ -33,44 +33,6 @@ class BookingPage(TemplateView):
 class ConfirmationPage(TemplateView):
 	template_name = 'booking/confirmation.html'
 
-	#TODO: SET CORRECT URLS
-	# @csrf_exempt
-	# def post(self, request):
-	# 	publishKey = settings.STRIPE_PUBLISHABLE_KEY
-	# 	print('test')
-	# 	if request.method == 'POST':
-	# 		print('test')
-	# 		token = request.POST.get('stripeToken', False)
-	# 		print('test')
-	# 		if token:
-	# 			print('test1')
-	# 			try:
-	# 				print('test2')
-	# 				# Create a Customer:
-	# 				customer = stripe.Customer.create(
-	# 					source=token,
-	# 					email='paying.user@example.com',
-	# 				)
-	#
-	# 				# Charge the Customer instead of the card:
-	# 				charge = stripe.Charge.create(
-	# 					amount=1000,
-	# 					currency='aud',
-	# 					customer=customer.id,
-	# 				)
-	# 				return redirect(reverse('home',
-	# 						kwargs={
-	# 							'token': token
-	# 						})
-	# 					)
-	# 			except stripe.CardError as e:
-	# 				message.info(request, "Your card has been declined.")
-	# 	context = {
-	# 		'STRIPE_PUBLISHABLE_KEY': publishKey
-	# 	}
-	#
-	# 	return render(request, self.template_name, context)
-
 	def get(self, request):
 		print("GET METHOD")
 
@@ -88,7 +50,7 @@ class ConfirmationPage(TemplateView):
 class SuccessPage(TemplateView):
 	template_name = 'booking/success.html'
 
-	def post(self, request): # new
+	def post(self, request):
 		if request.method == 'POST':
 			print('test')
 			token = request.POST.get('stripeToken', False)
