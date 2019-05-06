036f11a712c25b9fafdcb83c78756bed6e7758cc
TyuioPi <j17tan@gmail.com>
2019-05-06 20:45:03 +1000

    Add booking model

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Our Booking Model
+class Booking(models.Model):
+	user = models.ForeignKey(User, on_delete=models.SET_NULL)
+	start_date = models.DateField(auto_now_add=True)
+	start_time = models.TimeField(auto_now_add=True)
+	end_date = models.DateField()
+	end_time = models.TimeField()
+	
+	# Customer id for payment
+	customer_id = models.CharField()
+
+	# Static car details for the booking
+	brand = models.CharField(max_length=20)
+	transmission = models.CharField(max_length=20)
+	number_plate = models.CharField(max_length=7)
+	price = models.IntegerField()
+	longitude = models.FloatField()
+	latitude = models.FloatField()
