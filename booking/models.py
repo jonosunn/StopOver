5e5b0ab54951c4565f94f4b1ae36ad7b9727b5cb
TyuioPi <j17tan@gmail.com>
2019-05-06 20:47:52 +1000

    Fix booking model errors

--- booking/models.py
+++ booking/models.py
@@ -3,14 +3,14 @@ from django.contrib.auth.models import User
 
 # Our Booking Model
 class Booking(models.Model):
-	user = models.ForeignKey(User, on_delete=models.SET_NULL)
+	user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 	start_date = models.DateField(auto_now_add=True)
 	start_time = models.TimeField(auto_now_add=True)
 	end_date = models.DateField()
 	end_time = models.TimeField()
-	
+
 	# Customer id for payment
-	customer_id = models.CharField()
+	customer_id = models.CharField(max_length=250)
 
 	# Static car details for the booking
 	brand = models.CharField(max_length=20)
