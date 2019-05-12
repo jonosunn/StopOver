c7b2f34ab0c79cab5594679575bd190bc00f6eb4
s3432733 <s3432733@student.rmit.edu.au>
2019-05-12 15:57:01 +1000

    custom view

--- map/admin.py
+++ map/admin.py
@@ -3,4 +3,10 @@ from django.contrib import admin
 # Register your models here.
 from .models import Car
 
-admin.site.register(Car)
+admin.site.site_header = 'Admin Dashboard'
+
+class CarAdmin(admin.ModelAdmin):
+    view_on_site = False
+    change_list_template = 'admin/preview_template.html'
+
+admin.site.register(Car, CarAdmin)
