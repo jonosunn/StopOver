151cec849f2adbd55dfcf05b3c55d191751c4372
NotKhoa <khoa.nicky@gmail.com>
2019-05-13 21:33:33 +1000

    url auth

--- booking/views.py
+++ booking/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from map.models import Car
+from user.models import Account
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView, View
 from django.views.generic.list import ListView
+from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
 from decimal import Decimal
-from django.views.decorators.csrf import csrf_exempt
 from django.urls import reverse
-from django.contrib.auth.decorators import login_required
 import stripe
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
@@ -17,35 +19,49 @@ class BookingPage(TemplateView):
 
 	# Recieving get request from form
 	def get(self, request, number_plate):
-		# Set car object using the number_plate
-				
-		set_car = Car.objects.get(number_plate=number_plate)
-		# Set the selected car to false so other users can't select the car
-		# set_car.available = False
-		# Save car object to database
-		# set_car.save()
+		# if user book status is false, continue, else alert user
 
-		args = {
-        	"car": set_car,
-    	}
+		account_user = Account.objects.get(user__username=request.user)
+		if account_user.book_status == False:
+			# Set car object using the number_plate
+			set_car = Car.objects.get(number_plate=number_plate)
+			# Set the selected car to false so other users can't select the car
+			# set_car.available = False
+			# Save car object to database
+			# set_car.save()
+			args = {
+	        	"car": set_car,
+	    	}
+		else:
+			# User has booked a car already, send an alert
+			return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+
 		return render(request, self.template_name, args)
 
 
 class ConfirmationPage(TemplateView):
 	template_name = 'booking/confirmation.html'
 
+	def get(self, request):
+		# when user enters url instead through booking, redirect to homepage
+		return redirect(reverse('home'))
+
 	def post(self, request):
-		print("GET METHOD")
-
-		# Get the number plate posted
-		number_plate = request.POST.get("number_plate", "value")
-
-		# Get car object using number plate
-		booked_car = Car.objects.get(number_plate=number_plate)
-		args = {
-			"car": booked_car
-		}
-
+		print("POST METHOD")
+		# Get user book status to see if they have booked a car
+		account_user = Account.objects.get(user__username=request.user)	
+		if account_user.book_status == False:	# if false, they can't book
+			# Get the number plate posted
+			number_plate = request.POST.get("number_plate", "value")
+			# Get car object using number plate
+			booked_car = Car.objects.get(number_plate=number_plate)
+			args = {
+				"car": booked_car
+			}
+		else:
+			return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+			# User has booked a car already... Alert.
 		return render(request, self.template_name, args)
 
 class SuccessPage(TemplateView):
