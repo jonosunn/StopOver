1f50fd2a27996ccbf32899c502015e4f5d54a6af
apcteo <anapcteo@gmail.com>
2019-05-13 21:14:31 +1000

    Remove booking history page

--- user/urls.py
+++ user/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
-from .views import UserDashPage, BookingHistoryPage, RegisterPageView, UserDashPage, LoginPageView
+from .views import UserDashPage, RegisterPageView, UserDashPage, LoginPageView
 
 urlpatterns = [
     path('register/', RegisterPageView.as_view(), name='register'),
     path('login/', LoginPageView.as_view(template_name='user/login.html'), name='login'),
     path('user', UserDashPage.as_view(), name='user'),
-    path('history', BookingHistoryPage.as_view(), name='history'),
+    # path('history', BookingHistoryPage.as_view(), name='history'),
 ]
