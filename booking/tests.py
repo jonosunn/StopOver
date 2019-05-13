a7f79730871581389797628b0944cfacc6cb44b9
NotKhoa <khoa.nicky@gmail.com>
2019-05-13 23:53:45 +1000

    fix test

--- booking/tests.py
+++ booking/tests.py
@@ -11,6 +11,7 @@ class MapAppTest(TestCase):
     def setUp(self):
         Car.objects.create(brand='test_brand', transmission='automatic', number_plate='TEST01',
             price=100, longitude=-37.6799703, latitude=145.0548504, available=True)
+        self.client.force_login(self.user)
 
     def test_booking_page_status_code(self):
         car = Car.objects.get(id=1)
