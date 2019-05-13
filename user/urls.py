613892d30d21eaaba51e588cf85666e489f9b010
NotKhoa <khoa.nicky@gmail.com>
2019-05-14 01:23:48 +1000

    fixed user/url merge conflict

--- user/urls.py
+++ user/urls.py
@@ -6,12 +6,7 @@ from .views import UserDashPage, RegisterPageView, UserDashPage, LoginPageView
 
 urlpatterns = [
     path('register/', RegisterPageView.as_view(), name='register'),
-<<<<<<< HEAD
     path('login/', LoginPageView.as_view(template_name='user/login.html', redirect_authenticated_user=True), name='login'),
-    path('user', login_required(UserDashPage.as_view()), name='user'),
+    path('user/', login_required(UserDashPage.as_view()), name='user'),
 
-=======
-    path('login/', LoginPageView.as_view(template_name='user/login.html'), name='login'),
-    path('user/', UserDashPage.as_view(), name='user'),
->>>>>>> 4e1419f8f10e787cd7ec9fc2d06b653e9ede002f
 ]
