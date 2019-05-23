1adcca86fcf9f6bab771d1ce18ff2f9990556729
apcteo <anapcteo@gmail.com>
2019-05-23 20:56:47 +1000

    Edit about page styling, add a slideshow + template

--- map/urls.py
+++ map/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views
-from .views import HomePageView, SimulationPageView
+from .views import HomePageView, SimulationPageView, AboutPageView
 from django.contrib.admin.views.decorators import staff_member_required
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
-    path('admin/car/simulation', staff_member_required(SimulationPageView.as_view()), name='simulation')
+    path('admin/car/simulation', staff_member_required(SimulationPageView.as_view()), name='simulation'),
+    path('about/', AboutPageView.as_view(), name='about')
 ]
-
