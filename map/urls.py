f48e6e9456f59861967ba4d6e432fefc2193e78a
apcteo <anapcteo@gmail.com>
2019-05-26 17:39:47 +1000

    Add about/ url in map/url

--- map/urls.py
+++ map/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
-from .views import HomePageView, SimulationPageView, HelpPageView
+from .views import HomePageView, SimulationPageView, HelpPageView, AboutPageView
 from django.contrib.admin.views.decorators import staff_member_required
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('admin/car/simulation', staff_member_required(SimulationPageView.as_view()), name='simulation'),
-    path('help/', HelpPageView.as_view(), name='help')
+    path('help/', HelpPageView.as_view(), name='help'),
+    path('about/', AboutPageView.as_view(), name='about')
 ]
